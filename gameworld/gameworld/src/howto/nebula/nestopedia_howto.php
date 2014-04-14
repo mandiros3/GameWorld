eb05301c61f6d6dd79f9cf66701ccf883ac3c6f8
mandiros3 <Mandirosa3@gmail.com>
2014-04-13 21:28:13 -0400

    Reupdated everything to the latest version. Push as you develop

---
+++ gameworld/gameworld/src/howto/nebula/nestopedia_howto.php
@@ -0,0 +1,456 @@
+<!-- This is the project: Gameworld.
+
+authors: Andi Milhomme, Sybunlong Heng, Sereyrath Nov -
+
+-->
+<!DOCTYPE html>
+
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>How To</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width">
+
+		<!-- Bootstrap core CSS -->
+		<link rel="stylesheet" href="../../../css/bootstrap.min.css">
+		<link rel="stylesheet" href="../../../css/bootstrap-theme.min.css">
+
+		<!--  custom CSS here -->
+		<link rel="stylesheet" href="../../../css/main.css">
+		
+		 <!-- IMPORTANT:  PUT ALL JAVASCRIPT TAGS AT THE BOTTOM -->
+		
+		<script style="text/javascript">
+			var cf = new ContentFlow('contentFlow', {
+				reflectionColor : "#000000"
+			});
+		</script>
+
+		<!-- Google Font -->
+		<link href='http://fonts.googleapis.com/css?family=Parisienne|Josefin+Sans|Didact+Gothic|Cinzel' rel='stylesheet' type='text/css'>
+		
+	</head>
+
+	<body bgcolor="#FFFFFF">
+
+		<div class="wrapper">
+
+			<?php
+			include '../../../php/includes/navbar.php'; ?>
+		
+		
+			<p id="title">
+				Nestopia
+			</p>
+			<hr>
+
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<!-- ------------------------------------------------------------------------------------------------------------------------------------------------------ -->
+						<!-- ===== COVER FLOW ===== -->
+						<div id="contentFlow" class="ContentFlow">
+							<!-- should be place before flow so that contained images will be loaded first -->
+							<div class="loadIndicator">
+								<div class="indicator"></div>
+							</div>
+							<div class="flow">
+								<div class="item">
+									<img class="content" src="../../../img/nes/1.jpg"class="img-rounded"/>
+									<div class="caption">
+										pic0: some stripes
+									</div>
+								</div>
+								<div class="item">
+									<img class="content" src="../../../img/nes/2.jpg"class="img-rounded"/>
+									<div class="caption">
+										pic1: some stripes
+									</div>
+								</div>
+								<div class="item">
+									<img class="content" src="../../../img/nes/3.jpg"class="img-rounded"/>
+									<div class="caption">
+										pic2: some stripes
+									</div>
+								</div>
+								<div class="item">
+									<img class="content" src="../../../img/nes/4.jpg"class="img-rounded"/>
+									<div class="caption">
+										pic1: some stripes
+									</div>
+								</div>
+								<div class="item">
+									<img class="content" src="../../../img/nes/5.jpg"class="img-rounded"/>
+									<div class="caption">
+										pic0: some stripes
+									</div>
+								</div>
+								<div class="item">
+									<img class="content" src="../../../img/nes/6.jpg"class="img-rounded"/>
+									<div class="caption">
+										pic1: some stripes
+									</div>
+								</div>
+								<div class="item">
+									<img class="content" src="../../../img/nes/7.jpg"class="img-rounded"/>
+									<div class="caption">
+										pic2: some stripes
+									</div>
+								</div>
+								<div class="item">
+									<img class="content" src="../../../img/nes/8.jpg"class="img-rounded"/>
+									<div class="caption">
+										pic1: some stripes
+									</div>
+								</div>
+								<div class="item">
+									<img class="content" src="../../../img/nes/9.jpg"class="img-rounded"/>
+									<div class="caption">
+										pic0: some stripes
+									</div>
+								</div>
+								<div class="item">
+									<img class="content" src="../../../img/nes/10.jpg" class="img-rounded"/>
+									<div class="caption">
+										pic1: some stripes
+									</div>
+								</div>
+								<div class="globalCaption"></div>
+								<div class="scrollbar">
+									<div class="slider">
+										<div class="position"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- ------------------------------------------------------------------------------------------------------------------------------------------------------ -->
+				<hr>
+				<!-- ------------------------------------------------------------------------------------------------------------------------------------------------------ -->
+				<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="panel panel-primary">
+							<div class="panel-heading">
+								<h3 class="panel-title">Requirements</h3>
+								
+							</div>
+				
+				<!-- ===== Magnify Photo ===== -->
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="mag">
+								<h2 class="stepTitle"> Computers </h2>
+								<br>
+								<a href="http://bestbuy.com/" class="btn btn-default btn-success btn-xs active" target="_blank"> Link</a>
+
+								<img data-toggle="magnify" src="../../../img/prereq/1.png" alt="">
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-4">
+							<div class="mag">
+								<h2 class="stepTitle"> Controller </h2>
+								<br />
+								<a href="http://www.ebay.com/itm/black-twin-shock-game-controller-joypad-for-sony-playstation-2-ps2-/111217527330?pt=US_Video_Game_Controllers&hash=item19e514ca22" class="btn btn-default btn-success btn-xs active" target="_blank" > Link</a>
+								<img data-toggle="magnify" src="../../../img/prereq/2.png" alt="">
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-4">
+							<div class="mag">
+								<h2 class="stepTitle"> Adapters </h2>
+								<br>
+								<a href="http://www.ebay.com/itm/PS1-PS2-PSX-to-PC-USB-Dual-Controller-Adapter-Converter-/170854914674?pt=US_Video_Game_Cables_Adapters&hash=item27c7bf1a72" class="btn btn-default btn-success btn-xs active" target="_blank"> Link</a>
+								<img data-toggle="magnify" src="../../../img/prereq/3.png" alt="">
+							</div>
+						</div>
+						<!--/span-->
+					</div>
+					<!--/row-->
+					
+				</div>
+			</div>
+		</div>
+
+		<br>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<h3 class="panel-title">Instructions</h3>
+							<span class="pull-right"> <!-- Tabs -->
+								<ul class="nav panel-tabs">
+									<li class="active">
+										<a href="#tab1" data-toggle="tab">Details</a>
+									</li>
+									<li>
+										<a href="#tab2" data-toggle="tab">FAQ</a>
+									</li>
+									<li>
+										<a href="#tab3" data-toggle="tab">Contact <!-- <em style="color:red;"> (Registered users only)</em> --></a>
+									</li>
+
+								</ul> </span>
+						</div>
+						<div class="panel-body">
+							<div class="tab-content">
+								<div class="tab-pane active" id="tab1">
+									<!-- ------------------------------------------------------------------------------------------------------------------------------------------------------ -->
+									<!-- ===== Steps ===== -->
+									<br>
+									<br>
+									<br>
+									<br>
+									<div class="stepwizard">
+										<div class="stepwizard-row">
+											<div class="stepwizard-step">
+												<button type="button" class="btn btn-primary btn-circle">
+													1
+												</button>
+												<p>
+													Emulator
+												</p>
+											</div>
+											<div class="stepwizard-step">
+												<button type="button" class="btn btn-default btn-circle">
+													2
+												</button>
+												<p>
+													Roms
+												</p>
+											</div>
+											<div class="stepwizard-step">
+												<button type="button" class="btn btn-default btn-circle">
+													3
+												</button>
+												<p>
+													Controller
+												</p>
+											</div>
+						
+											
+										</div>
+									</div>
+									<br>
+									<br>
+									<br>
+									<br>
+									<hr>
+									<br>
+									<br>
+									<br>
+									<br>
+
+									<p>
+										<b>1.Get The Emulator</b>
+									</p>
+
+									<p>
+										· Download Nestopia Latest Version <a href="http://nestopia.sourceforge.net" target="_blank" >http://nestopia.sourceforge.net</a>
+									</p>
+									<p>
+										· Install Nestopia
+									</p>
+									
+									<br>
+									<br>
+						
+									<hr>
+							
+									<br>
+									<br>
+
+									<p>
+										<b>2. Get The Roms</b>
+									</p>
+
+									<p>
+										· Download Roms at <a href="http://coolrom.com/roms/nes/" target="_blank" >http://coolrom.com/roms/nes/</a>
+									</p>
+									
+									<p>
+									
+									<a href="#" class="btn btn-sm btn-danger"><span class="glyphicon glyphicon-remove"></span> Warning</a>
+									
+									
+										Please only download the roms that you own.
+									</p>
+
+									<br>
+									<br>
+						
+									<hr>
+							
+									<br>
+									<br>
+
+									<p>
+										<b>3. Load the downloaded roms </b>
+									</p>
+
+									<p>
+										· File > Open > Browse for the '.NES' file that you just downloaded.
+									</p>
+
+									
+									<br>
+									<br>
+						
+									<hr>
+							
+									<br>
+									<br>
+
+									<p>
+										<b>4. Configure the Controller</b>
+									</p>
+
+									<p>
+										· Load The Game First
+									</p>
+
+									<p>
+										· Goto <b>Option</b> &gt; <b>Input</b> &gt; <b>Player 1</b>
+									</p>
+
+									
+									<p>
+										<b>Cheat Code</b>
+									</p>
+
+									<p>
+										<a href="http://www.gamefaqs.com" target="_blank" >http://www.gamefaqs.com</a>
+									</p>
+
+								</div>
+								<div class="tab-pane" id="tab2">
+
+									<p>
+										<b>Q: I can’t load my roms. What did I do wrong ?</b>
+									</p>
+
+									<p>
+										A: Did you install the bio yet ? If not, please do so.
+									</p>
+
+									<p>
+										<b>Q: Do we need to unzip the ROM after download them?</b>
+									</p>
+
+									<p>
+										A: No, you don't need to. You just need to place them in the right folder.
+									</p>
+
+									<p>
+										<b>Q: How do I configure the controller to work with Nebula ?</b>
+									</p>
+
+									<p>
+										A: First, you need an adapter and a PS2 controller or any other type of controller is fine. Then, you just need to follow the fourth step in the details. tab.
+									</p>
+								</div>
+								<div class="tab-pane" id="tab3">
+
+									<div class="container">
+										<div class="row">
+											<div class="col-sm-4">
+												<h3>Drop us a line!</h3>
+												<hr>
+												<address>
+													<strong>Bunlong Heng</strong>
+													<br>
+													<strong>Email:</strong><a href="mailto:#"> Bunlong_Heng@student.uml.edu</a>
+													<br>
+													<strong>Phone:</strong> (978)677-0861
+												</address>
+												<address>
+													<strong>Andi Milhhome</strong>
+													<br>
+													<strong>Email:</strong><a href="mailto:#"> Andi_Milhhome@student.uml.edu</a>
+													<br>
+													<strong>Phone:</strong> (617)820-3596												</address>
+												<address>
+													<strong>Sereyrath Nov</strong>
+													<br>
+													<strong>Email:</strong><a href="mailto:#"> Sereyrath_Nov@student.uml.edu</a>
+													<br>
+													<strong>Phone:</strong>(781)215-3158
+												</address>
+
+											</div>
+
+											<div class="col-sm-7 contact-form">
+												<form id="contact" method="post" action="contactformprocess_gameworld.php" class="form" role="form">
+													<div class="row">
+														<div class="col-xs-6 col-md-6 form-group">
+															<input class="form-control" id="name" name="name" placeholder="Name" type="text" required autofocus />
+														</div>
+														<div class="col-xs-6 col-md-6 form-group">
+															<input class="form-control" id="email" name="email" placeholder="Email" type="email" required />
+														</div>
+														<div class="col-xs-6 col-md-6 form-group">
+															<input class="form-control" id="subject" name="subject" placeholder="Subject" type="subject" required />
+														</div>
+													</div>
+													<textarea class="form-control" id="message" name="message" placeholder="Message" rows="5"></textarea>
+													<br />
+													<div class="row">
+														<div class="col-xs-12 col-md-12 form-group">
+															<button class="btn btn-primary pull-right" type="submit">
+																Submit
+															</button>
+												</form>
+											</div>
+										</div>
+
+									</div>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- ------------------------------------------------------------------------------------------------------------------------------------------------------ -->
+			<!-- ===== Footer ===== -->
+			<hr>
+			<footer>
+				<p>
+					&copy; GameWorld 2014
+				</p>
+			</footer>
+		</div>
+		<!--wrapper-->
+
+	</body>
+	
+	
+	<!--Javascript dependencies and other libraries such as jquery UI-->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+	<script>
+		window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>');
+	</script>
+	<script src="../../../js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+
+	<!--Javascript responsible for responsivness -->
+	<script src="../../../js/vendor/bootstrap.min.js"></script>
+	<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+
+	<!-- custom javascript-->
+	<script src="../../../js/main.js"></script>
+	
+	<!-- 	ContentFlow -->
+		<script language="JavaScript" type="text/javascript" src="../../../js/ContentFlow/contentflow.js"></script>
+		<script language="JavaScript" type="text/javascript" src="../../../js/magnify.js"></script>
+	
+	
+	
+</html>

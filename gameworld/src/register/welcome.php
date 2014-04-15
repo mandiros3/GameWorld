35aa6139aaf38ac8777ab50c8a9a4a852005a60e
mandiros3 <Mandirosa3@gmail.com>
2014-04-14 23:53:31 -0400

    welcome update

--- gameworld/src/register/welcome.php
+++ gameworld/src/register/welcome.php
@@ -10,51 +10,79 @@ if(!$_SESSION['UserName']){
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+	<!--<![endif]-->
 
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Profile</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width">
 
-   <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-      <title>Welcome Page</title>
-      <meta name="description" content="">
-      <meta name="viewport" content="width=device-width">
+		<!-- Bootstrap core CSS -->
+		<link rel="stylesheet" href="../../css/bootstrap.min.css">
+		<link rel="stylesheet" href="../../css/bootstrap-theme.min.css">
 
-      <!-- Bootstrap core CSS -->
-      <link rel="stylesheet" href="../../css/bootstrap.min.css">
+		<!--  custom CSS here -->
+		<link rel="stylesheet" href="../../css/main.css">
 
+		<!--Javascript responsible for responsivness -->
 
-      <style>
+		<script src="../../js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+	</head>
 
+	<!-- IMPORTANT:  PUT ALL JAVASCRIPT FILES AT THE BOTTOM -->
 
-         body {
-            padding-top: 50px;
-            padding-bottom: 20px;
-         }
-      </style>
-      <link rel="stylesheet" href="../../css/bootstrap-theme.min.css">
+	<body>
 
-      <!--  custom CSS here -->
-      <link rel="stylesheet" href="../../css/main.css">
-      <!--Javascript responsible for responsivness -->
+		<div class="wrapper">
+			<?php require("../../php/includes/navbar.php"); ?>
 
+			<div class="container">
+				<br>
+				<section class="row">
+					<div class="col-lg-3">
+						<h1>Welcome <?php echo $_SESSION['UserName']; ?>   </h1>
+						
+						<img src="img/profile/homer.jpg" alt="..." class="img-rounded">
+							
+					</div>
+					
+					<div class="col-lg-9">
+						
+						<p> Under development</p>
+					</div>
+						
+				</section>
+				
+				
+			</div>
+				</div>
+					</div>
 
-      <script src="../../js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-   </head>
+			<hr>
 
+			<footer>
+				<p>
+					&copy; GameWorld 2014
+				</p>
+			</footer>
 
-<style type='text/css'>
-   body {
-   background:url('NewYear2014.jpg');
-   }
-</style>
+			<!--Javascript dependencies and other libraries such as jquery UI-->
+			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+			<script>
+				window.jQuery || document.write('<script src="../../js/vendor/jquery-1.10.1.min.js"><\/script>');
+			</script>
+			<script src="../../js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 
-<body>    
-    	<?php require("../../php/includes/navbar.php"); ?>
+			<!--Javascript responsible for responsivness -->
+			<script src="../../js/vendor/bootstrap.min.js"></script>
+			<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 
-<b><br>Welcome<br></b>
+			<!-- custom javascript-->
+			<script src="../../js/main.js"></script>
 
-<font color='red' size='4'><?php echo $_SESSION['UserName']; ?></font>
-<h3 align='right'><a href='logout.php'>Logout Here</a></h3>
-</body>
+	</body>
 </html>

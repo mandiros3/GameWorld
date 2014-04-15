2718d8cd291c7411bc91b6c47b150ea6ad0f3b54
mandiros3 <Mandirosa3@gmail.com>
2014-04-15 01:05:48 -0400

    fixed a bunch of things in the background

--- gameworld/src/howto/nebula/nebula_howto.php
+++ gameworld/src/howto/nebula/nebula_howto.php
@@ -275,7 +275,7 @@ authors: Andi Milhomme, Sybunlong Heng, Sereyrath Nov -
 									
 									<p>
 									
-									<a href="#" class="btn btn-sm btn-danger"><span class="glyphicon glyphicon-remove"></span> Warning</a>
+									<p><span class="label label-danger glyphicon glyphicon-remove"> Warning</span>  </p>
 									
 									
 										Please only download the roms that you own.
